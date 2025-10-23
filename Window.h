c0fd8fa1c5d9846b7923b20fc9b4fc2eaf08812a
Rodrigo-GarPen <111661896+Rodrigo-GarPen@users.noreply.github.com>
2025-10-22 19:45:11 -0600

    Se agregan archivos principales
    Los modelos no los agrego porque algunos exceden el peso máximo

---
+++ Window.h
@@ -0,0 +1,51 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLfloat getmuevex2() { return muevex2; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	GLfloat getarticulacion1() { return articulacion1; }
+	GLfloat getarticulacion2() { return articulacion2; }
+	GLfloat getarticulacion3() { return articulacion3; }
+	bool shouldSwitch1() { return alterna1; }
+	bool shouldSwitch2() { return alterna2; }
+	bool getMoveForward() { return moveForward; }
+	bool getMoveBackwards() { return moveBackwards; }
+
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat muevex2;
+	GLfloat articulacion1, articulacion2, articulacion3;
+	bool alterna1, alterna2;
+	bool moveForward, moveBackwards;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
